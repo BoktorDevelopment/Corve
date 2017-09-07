cbbd7b7cca918b8ff59ca6fd2deb8a0b17844d00
MrKing95 <thomas@newheap.com>
2017-09-07 11:24:17 +0200

    EF updates

--- CorveTool/DAL/Context/DatabaseContext.cs
+++ CorveTool/DAL/Context/DatabaseContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CorveTool.DAL.Models;
+﻿using CorveTool.DAL.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CorveTool.DAL.Context
@@ -12,5 +8,9 @@ namespace CorveTool.DAL.Context
         public DbSet<Tasks> Tasks { get; set; }
         public DbSet<Schedules> Schedules { get; set; }
         public DbSet<ScheduleTask> Schedule_Task { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        { }
     }
 }
