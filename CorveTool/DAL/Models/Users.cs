faaa99cf6e36464c8db65dad727b3c39e403e82d
BoktorDevelopment <kris.broekstra@outlook.com>
2017-09-07 09:49:37 +0200

    solution upload

---
+++ CorveTool/DAL/Models/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace CorveTool.DAL.Models
+{
+    public class Users : IUserData
+    {
+        public int UserId { get ; set; }
+        public string FirstName { get; set ; }
+        public string Insertion { get ; set ; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string SlackName { get; set; }
+
+       
+       /* string IUserData.getSlackName()
+        {
+            return this.SlackName;
+            fddsfsad
+        }
+        */
+    }
+}
---
+++ CorveTool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CorveTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
